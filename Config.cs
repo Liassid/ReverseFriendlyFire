0f2763b25e8621f353f0eea0d46e01292294c9d8
Liassid <liassidarthur@gmail.com>
2024-04-23 21:27:38 +0500

    missing words

--- Config.cs
+++ Config.cs
@@ -20,9 +20,9 @@ namespace ReverseFriendlyFire
         public float DamageThreshold { get; set; } = 500f;
         [Description("What multipliers should be applied on kills and damages with specified Damage Types? E.g. 0.34 kill multiplier will result in Damage Reversing activating only on 3rd kill with SCP-018")]
         public Dictionary<DamageType, DamageTypeMultiplier> DamageTypeMultipliers { get; set; } = new() { { DamageType.Scp018, new() { Kills = 0.34f, Damage = 0.34f } } };
-        [Description("What death message should shown to player when dying due to Damage Reversing?")]
+        [Description("What death message should be shown to player when dying due to Damage Reversing?")]
         public string ReverseDamageReason { get; set; } = "Teamkilling";
-        [Description("What hint should shown to player when Damage Reversing is activated?")]
+        [Description("What hint should shown be to player when Damage Reversing is activated?")]
         public Hint ReverseDamageActivationHint { get; set; } = new() { Content = "<color=red>Reverse Friendly Fire activated</color>\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", Duration = 5f };
     }
 }
